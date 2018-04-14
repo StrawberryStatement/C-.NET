fb807713228b4050c4931cfb4d8785e6aeb6eb19
Trick150 <trick1500@163.com>
2018-04-14 10:54:45 +0800

    ver1.0 完成冒泡排序可视化
    冒泡排序可视化

--- Grapecity_Code/Bubble.cs
+++ Grapecity_Code/Bubble.cs
@@ -11,7 +11,6 @@ namespace Grapecity_Code
         List<int> initArray = new List<int>();
         List<int> locArray = new List<int>();        
         Queue<int[]> arrayState = new Queue<int[]>();
-        int sortedSeries;//交换排序中已确定好位置的元素。
         int swapCount = 0;
         /// <summary>
         /// 获取当前序列值
@@ -24,15 +23,8 @@ namespace Grapecity_Code
             }
         }
         /// <summary>
-        /// 获取已排序位置
+        /// 获取交换次数
         /// </summary>
-        public int getSortedSeries
-        {
-            get
-            {
-                return sortedSeries;
-            }
-        }
         public int getSwapCount
         {
             get
@@ -67,35 +59,32 @@ namespace Grapecity_Code
                 bool Change = false;
                 for (int j = i + 1; j < initArray.Count(); j++)
                 {
+                    initArray.ForEach(data => locArray.Add(data));
+                    locArray.Add(i);   //队列中加入需要交换的元素位置；
+                    locArray.Add(j);
                     if (initArray[j] < initArray[i])
                     {
+                        //交换前的元素进队
+                        arrayState.Enqueue(locArray.ToArray());//List 引用不能直传
+                        locArray.Clear();
                         //Swap
                         int temp = initArray[i];
                         initArray[i] = initArray[j];
                         initArray[j] = temp;
+                        //交换后的元素进队
                         initArray.ForEach(data => locArray.Add(data));
-                        locArray.Add(i);   //队列中加入需要交换的元素位置；
+                        locArray.Add(i);   
                         locArray.Add(j);
-                        arrayState.Enqueue(locArray.ToArray());//List 引用不能直传
-                        locArray.Clear();
                         swapCount++;
                         Change = true;
-
                     }
-                    else
-                    {
-                        initArray.ForEach(data => locArray.Add(data));
-                        locArray.Add(255);   //255表示不交换
-                        locArray.Add(255);
-                        arrayState.Enqueue(locArray.ToArray());//List 引用不能直传
-                        locArray.Clear();
-                    }
+                    arrayState.Enqueue(locArray.ToArray());//List 引用不能直传
+                    locArray.Clear();
                 }               
                 if (Change == false)
                 {
                     break;
                 }
-                sortedSeries=i;//一次外层排序确定一个最终位置。
             }
         }
     }
